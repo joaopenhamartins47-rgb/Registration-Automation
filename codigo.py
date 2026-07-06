3221b11ed37215575f8db1fb5fa745583a29b372
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-07-06 20:01:08 -0300

    initial commit

---
+++ codigo.py
@@ -0,0 +1,13 @@
+
+
+#Mexe no teu pc para automacoes
+import pyautogui
+
+# Passo 1: Entrar no sistema da empresa
+pyautogui.press("win")
+
+
+# Passo 2: Fazer login
+# Passo 3: Abrir a base de dados
+# Passo 4: Cadastrar 1 produto
+# Passo 5: Repetir o passo 4 ate acabar a lista de produtos
